92ad8cb3ebe19ea39bafb13b82d1a40b2b7a975a
Stefan von den Berken <stefan.vondenberken@yatta.de>
2024-08-01 00:11:33 +0200

    MP-6801 provide two different endpoints for internal iam and yatta iam

--- de.yatta.ideintruders.backend/src/main/java/de/yatta/ideintruders/backend/IdeIntrudersBackendApplication.java
+++ de.yatta.ideintruders.backend/src/main/java/de/yatta/ideintruders/backend/IdeIntrudersBackendApplication.java
@@ -12,8 +12,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
@@ -22,8 +25,6 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = { SecurityConfig.class })
@@ -46,25 +47,36 @@ public class IdeIntrudersBackendApplication
 
    @GetMapping("/queryLicense")
    @CrossOrigin(origins = "*")
-   public boolean queryLicense(@RequestParam(value = "sessionToken", required = false) String sessionToken, @RequestParam(value = "productId") String productId)
-         throws JsonProcessingException
+   public boolean queryLicense(
+         @RequestParam(value = "userId", required = false) String internalUserId,
+         @RequestParam(value = "productId", required = true) String productId)
    {
-
       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-      String accountId;
 
       if (authentication instanceof OAuth2AuthenticationToken token)
       {
-         accountId = token.getName();
-      }
-      else
-      {
-         var jwt = getDecodedJwt(sessionToken);
-         accountId = jwt.getSubject();
-         productId = jwt.getClaimAsString("productId");
+         // In case OAuth is used, we can assume the linked account id
+         // is the same as the internal user id.
+         internalUserId = token.getName();
       }
 
+      return checkLicenseServer(internalUserId, productId);
+   }
+
+   @PostMapping("/queryLicenseSession")
+   @CrossOrigin(origins = "*")
+   public boolean queryLicense(@RequestBody LicenseQuery query)
+   {
+      var jwt = getDecodedJwt(query.sessionToken());
+
+      String accountId = jwt.getSubject();
+      String productId = jwt.getClaimAsString("productId");
+
+      return checkLicenseServer(accountId, productId);
+   }
+
+   private boolean checkLicenseServer(String accountId, String productId)
+   {
       RestTemplate restTemplate = new RestTemplate();
 
       HttpHeaders headers = new HttpHeaders();
@@ -74,29 +86,27 @@ public class IdeIntrudersBackendApplication
       JSONObject jsonObject = new JSONObject();
       jsonObject.put("environment", "preview");
       jsonObject.put("productId", productId);
-      //jsonObject.put("featureId", "de.softwarevendor.product");
-      //jsonObject.put("version", "1.0.0");
       jsonObject.put("accountId", accountId);
       jsonObject.put("durationMinutes", 120);
 
-      HttpEntity<String> request = new HttpEntity<String>(jsonObject.toString(), headers);
+      HttpEntity<String> licenseRequest = new HttpEntity<>(jsonObject.toString(), headers);
 
       String result;
       try
       {
-         result = restTemplate.postForObject(licenseEndpoint, request, String.class);
+         result = restTemplate.postForObject(licenseEndpoint, licenseRequest, String.class);
+         ObjectMapper mapper = new ObjectMapper();
+         JsonNode tree = mapper.readTree(result);
+         System.out.println(tree.get("validity").asText());
+
+         return tree.get("validity").asText().equals("LICENSED");
       }
-      catch (RestClientException e)
+      catch (RestClientException | JsonProcessingException e)
       {
          System.out.println("Not Found: " + e.getMessage());
-         return false;
       }
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode tree = mapper.readTree(result);
 
-      System.out.println(tree.get("validity").asText());
-
-      return tree.get("validity").asText().equals("LICENSED");
+      return false;
    }
 
    private Jwt getDecodedJwt(String sessionToken)
